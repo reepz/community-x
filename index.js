9befb7453e1f4b79cd4218a064aedf8b2087c135
reepz <a.wojtowicz@outlook.com>
2019-01-07 12:37:56 +0100

    connected to PSQL, set env var TIMES

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const { Pool } = require('pg')
+const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.send.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: true
-})
+});
+
 
 const cool = require('cool-ascii-faces')
 const express = require('express')
